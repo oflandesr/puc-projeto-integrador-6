284bf2701309fe0e31b5db7cc7dbef40158ede06
gabrielhsdev <henriquegabrielhs21@gmail.com>
2024-10-03 15:08:03 -0300

    more stuff

--- app/frontend/frontend/app/page.tsx
+++ app/frontend/frontend/app/page.tsx
@@ -4,6 +4,8 @@ import CustomInput from "@/components/CustomInput";
 import React, {useEffect, useState} from "react";
 import useLazyGet from "@/hooks/useLazyGet";
 import CustomButton from "@/components/CustomButton";
+import {useUser} from "@/userContext";
+import {useRouter} from "next/navigation";
 import { Bars } from 'react-loading-icons'
 
 export default function Home() {
@@ -11,6 +13,10 @@ export default function Home() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [urlUser, setUrlUser] = useState<string>("");
+
+    // Context
+    const { updateUserData } = useUser();
+    const router = useRouter();
 
     const {
         data,
@@ -26,24 +32,49 @@ export default function Home() {
         fetchData: fetchUser
     } = useLazyGet(urlUser);
 
+    function resetForm() {
+        setEmail("");
+        setPassword("");
+    }
+
     async function handleSubmit() {
+        if (!email || !password) {
+            alert("Email and password are required");
+            return;
+        }
         await fetchData();
         if (!error) {
             console.log("Login successful", data);
             setUrlUser(`/user/${email}`);
         } else {
             console.log("Oops, something went wrong", error);
+            resetForm();
         }
     }
 
     useEffect(() => {
         const fetchData = async () => {
             if (urlUser) {
-                await fetchUser();
+                await fetchUser().then(() => {
+                    updateUserData(userData);
+                });
+                // await router.push("/dashboard");
+            } else {
+                console.log("No user data to fetch");
             }
         };
         fetchData();
     }, [urlUser]);
+
+    useEffect(() => {
+        if (userData) {
+            console.log("User data fetched", userData);
+            router.push("/dashboard");
+        } else {
+            console.log("No user data to fetch");
+            resetForm();
+        }
+    }, [userData]);
 
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
