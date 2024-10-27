0ad83a4f848aeeb576a554046c93f9114f617b2a
Oflandes Rodrigues <oflandesrps@gmail.com>
2024-10-27 18:56:49 -0300

    Estrutura docker completa.

--- builder.sh
+++ builder.sh
@@ -7,7 +7,7 @@ main() {
 
     if [ -d "/${GIT_REPO_NAME}/app/" ]; then
         echo "Diretório do repositório encontrado. Atualizando com git pull..."
-        cd "/${GIT_REPO_NAME}" || exit
+        cd "/${GIT_REPO_NAME}" || exit 1
         
         # Captura a saída do comando git pull
         output=$(git pull origin "${GIT_BRANCH}")
@@ -18,19 +18,42 @@ main() {
             echo "O repositório já está atualizado."
         else
             echo "Atualizações encontradas. Executando Maven..."
-            cd "/${GIT_REPO_NAME}/app/backend" || exit
+            cd "/${GIT_REPO_NAME}/app/backend" || exit 1
             mvn clean package -DskipTests
         fi
     else
         echo "Clonando o repositório..."
         git clone -b "${GIT_BRANCH}" "https://${GIT_USER}:${GIT_TOKEN}@github.com/${GIT_USER}/${GIT_REPO_NAME}.git" "/${GIT_REPO_NAME}"
-        configure_mysql  # Configura o MySQL apenas no clone
-        cd "/${GIT_REPO_NAME}/app/backend" || exit
-        mvn clean package -DskipTests  # Compila a aplicação após o clone
+        
+        # Configura o ambiente Python
+        echo "Preparando ambiente Python..."
+        setup_python_env
+
+        # Executa Maven após o clone
+        echo "Executando Maven..."
+        cd "/${GIT_REPO_NAME}/app/backend" || exit 1
+        mvn clean package -DskipTests
     fi
 
-    populate_mysql   # Chama a função para popular o MySQL
+    upsert_database   # Chama a função para popular o MySQL
     application_run  # Chama a função para executar a aplicação
+}
+
+# Função para configurar o ambiente Python
+setup_python_env() {
+    if command -v python3 &> /dev/null; then
+        if [ ! -d "/venv" ]; then
+            python3 -m venv /venv
+            echo "Ambiente virtual Python criado."
+        else
+            echo "Ambiente virtual Python já existe. Pulando criação."
+        fi
+        source /venv/bin/activate
+        pip install --no-cache-dir -r "/${GIT_REPO_NAME}/scripts/python/requirements.txt"
+    else
+        echo "Python3 não está disponível. Instale o Python3 e tente novamente."
+        exit 1
+    fi
 }
 
 # Função para aguardar a conexão com o MySQL
@@ -38,7 +61,7 @@ wait_for_mysql() {
     echo "Aguardando conexão com o MySQL..."
     MAX_ATTEMPTS=12
     attempts=0
-    until mysql -h "${MYSQL_HOST}" -uroot -p"${MYSQL_PASSWORD}" -e "SELECT 1;" > /dev/null 2>&1; do
+    until mysql -h "${MYSQL_HOST}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SELECT 1;" > /dev/null 2>&1; do
         if (( attempts++ >= MAX_ATTEMPTS )); then
             echo "Não foi possível conectar ao MySQL após $MAX_ATTEMPTS tentativas. Saindo..."
             exit 1
@@ -50,39 +73,29 @@ wait_for_mysql() {
 }
 
 # Função para configurar o MySQL
-configure_mysql() {
-    wait_for_mysql  # Aguarda conexão antes de configurar
-    echo "Configurando usuário MYSQL para o backend..."
-
-    mysql -h "${MYSQL_HOST}" -uroot -p"${MYSQL_PASSWORD}" <<EOF
-    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-    CREATE USER IF NOT EXISTS 'projiv'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
-    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO 'projiv'@'%';
-    FLUSH PRIVILEGES;
-EOF
-
-    echo "MySQL configurado e inicializado."
-}
-
-# Função para popular o MySQL
-populate_mysql() {
+upsert_database() {
+    echo "Iniciando operações com o MySQL..."
     wait_for_mysql  # Aguarda conexão antes de popular
 
-    echo "Executando scripts Python para setup do banco de dados..."
-    if [ ! -d "/venv" ]; then
-        python3 -m venv /venv
-    fi
-    source /venv/bin/activate
-
-    echo "Executando scripts..."
-    pip install --no-cache-dir -r "/${GIT_REPO_NAME}/scripts/python/requirements.txt"
+    echo "Executando scripts Python para o banco de dados..."
     
+    # Ativa o ambiente virtual se existir
+    if [ -d "/venv" ]; then
+        source /venv/bin/activate
+    else
+        echo "Ambiente virtual Python não encontrado. Verifique a configuração do Python."
+        exit 1
+    fi
+
     if [ ! -f "/${GIT_REPO_NAME}/.mysql_initialized" ]; then
+        echo "Criando tabelas e usuários..."
         python3 "/${GIT_REPO_NAME}/scripts/python/create_tables.py"
         touch "/${GIT_REPO_NAME}/.mysql_initialized"
     fi
 
+    echo "Atualizando os dados..."
     python3 "/${GIT_REPO_NAME}/scripts/python/etl.py"  # Sempre executa o ETL
+    echo "Operações com o banco de dados finalizadas"
 }
 
 # Função para compilar e executar a aplicação Java
