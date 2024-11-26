aa8c8e029e750327b84c8ffaa5d90e896abd53bb
gabrielhsdev <henriquegabrielhs21@gmail.com>
2024-11-26 20:38:28 -0300

    Test

--- scripts/python/lists.py
+++ scripts/python/lists.py
@@ -1,5 +1,31 @@
 TICKER_LIST = {
-    'PETR4': 'Petrobras'
+    'PETR4': 'Petrobras',
+    'VALE3': 'Vale',
+    'ITUB4': 'Itaú Unibanco',
+    'BBDC4': 'Banco Bradesco',
+    'ABEV3': 'Ambev',
+    'B3SA3': 'B3 S.A.',
+    'BBAS3': 'Banco do Brasil',
+    'MGLU3': 'Magazine Luiza',
+    'WEGE3': 'Weg',
+    'GGBR4': 'Gerdau',
+    'LREN3': 'Lojas Renner',
+    'SUZB3': 'Suzano',
+    'JBSS3': 'JBS',
+    'ELET3': 'Eletrobras ON',
+    'ELET6': 'Eletrobras PNB',
+    'TAEE11': 'Taesa',
+    'EQTL3': 'Equatorial Energia',
+    'GOLL4': 'Gol Linhas Aéreas',
+    'HYPE3': 'Hypera',
+    'CSNA3': 'CSN',
+    'KNRI11': 'Kinea Renda Imobiliária FII',
+    'HGLG11': 'CSHG Logística FII',
+    'XPML11': 'XP Malls FII',
+    'VISC11': 'Vinci Shopping Centers FII',
+    'HGBS11': 'Hedge Brasil Shopping FII',
+    'FEXC11': 'BTG Pactual Fundo de Fundos FII',
+    'MXRF11': 'Maxi Renda FII'
 }
 
 INDEX_LIST = {
